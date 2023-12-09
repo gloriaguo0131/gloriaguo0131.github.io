562f3570d059cb44069ae42ff28a3291e1e4972d
gloriaguo0131 <145466205+gloriaguo0131@users.noreply.github.com>
2023-12-09 18:27:17 -0500

    1209 edit

---
+++ app.js
@@ -0,0 +1,45 @@
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev')
+let nightskyDom = document.querySelector('.nightsky')
+let listItemDom = document.querySelector('.nightsky .list')
+let thumbnailDom = document.querySelector('.nightsky .thumbnail')
+
+nextDom.onclick = function(){
+    showSlider('next');
+}
+prevDom.onclick = function(){
+    showSlider('');
+}
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+let runTimeOut;
+let runAutoRun = setTimeout(()=>{
+    nextDom.click();
+}, timeAutoNext);
+
+function showSlider(type){
+    let itemSlider = document.querySelectorAll('.nightsky .list .item');
+    let itemaThumbnail = document.querySelectorAll('.nightsky .thumbnail .item');
+
+    if(type === 'next'){
+        listItemDom.appendChild(itemSlider[0]);
+        thumbnailDom.appendChild(itemThumnbnail[0]);
+        nightskyDom.classList.add('next');
+    }else{
+        let positionLastItem = itemSlider.length - 1;
+        listItemDom.prepend(itemSLider[positionLastItem]);
+        thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+        nightskyDom.classList.add('prev');
+    }
+
+    clearTimeOut(runTimeOut);
+    runTimeOut = setTimeout(() =>{
+        nightskyDom.classList.remove('next');
+        nightskyDom.classList.remove('prev');
+    }, timeRunning)
+
+    clearTimeout(runAutoRun);
+    runAutoRun = setTimeout(()=>{
+        nextDom.click();
+    }, timeAutoNext);
+}
